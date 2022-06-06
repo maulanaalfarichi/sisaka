a3e5d5a578342129f388a0eb07771cc6309eecd0
maulanaalfarichi <maulanaalfarichi.dava@gmail.com>
2022-06-06 13:16:33 +0700

    final

---
+++ public/app/main.js
@@ -0,0 +1,17 @@
+require.config({
+	baseUrl: "/app",
+	paths: {
+		// "webix": "/codebase/webix",
+		"menuTree":"views/webix/menutree",
+		"apps" : "apps"
+	},
+	/*"shim" : {
+		"menuTree" : ["webix"],
+		"apps" : ["webix","menuTree"]
+	}*/
+});
+
+/*require(['apps'], function(app){
+	return app.webix();
+});
+*/
